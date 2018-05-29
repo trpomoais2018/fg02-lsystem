75bd82b65f2bba7c230b8da6aafec6a487aadd9b
Maxim <ya.max-m2013@yandex.ru>
2018-05-29 21:19:15 +0500

    Реализовано сохранение состояния

--- Dragon Curve/DragonCurve.js
+++ Dragon Curve/DragonCurve.js
@@ -7,46 +7,53 @@ canvas.width = document.body.offsetWidth;
 canvas.height = window.innerHeight;
 var sky = canvas.getContext('2d');
 var length = 15;
+var axiome = "FX";
+var currentSentence = axiome;
+var sentences = [axiome];
+var rules = {
+  "X": "X+YF+"
+  , "Y": "-FX-Y"
+}
+var keys = Object.keys(rules);
 
 function toRadiance(degree) {
-	return degree * Math.PI / 180;
+  return degree * Math.PI / 180;
 }
-
-var axiome = "FX";
-var sentence = axiome;
-var rules = {
-"X" : "X+YF+"
-,"Y" : "-FX-Y"
-}
-var keys=Object.keys(rules);
 
 function generate() {
+  currentSentence = sentences[sentences.length - 1];
   var nextSentence = "";
-  for (var i = 0; i < sentence.length; i++) {
-    var current = sentence.charAt(i);
+  for (var i = 0; i < currentSentence.length; i++) {
+    var currentChar = currentSentence.charAt(i);
     var found = false;
     for (var j = 0; j < keys.length; j++) {
-      if (current === keys[j]){
+      if (currentChar === keys[j]) {
         found = true;
         nextSentence += rules[keys[j]];
         break;
       }
     }
     if (!found)
-      nextSentence += current;
+      nextSentence += currentChar;
   }
-  sentence = nextSentence;
+  sentences.push(nextSentence);
+  currentSentence = nextSentence;
 }
 
 function turtle(n) {
-  for(var j=0;j<n;j++){
-    generate();
+  if (sentences.length > n)
+    currentSentence = sentences[n];
+  else {
+    let len = sentences.length;
+    for (var j = 0; j < n - len + 1; j++) {
+      generate();
+    }
   }
   sky.translate(600, 500);
   sky.lineWidth = "1";
   sky.strokeStyle = "green";
-  for (var i = 0; i < sentence.length; i++) {
-    var current = sentence.charAt(i);
+  for (var i = 0; i < currentSentence.length; i++) {
+    var current = currentSentence.charAt(i);
     if (current === "F") {
       sky.beginPath();
       sky.moveTo(0, 0);
@@ -57,7 +64,7 @@ function turtle(n) {
       sky.rotate(toRadiance(90));
     } else if (current === "-") {
       sky.rotate(toRadiance(-90));
-    } 
+    }
   }
 }
 turtle(10);
